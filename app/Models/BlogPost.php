082381fc25fa3da6e64d44c3a0b07defd15bb7ec
Michael Pollard <mickeymakesapps88@gmail.com>
2023-07-02 09:04:56 -0600

    updated repo name

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -9,7 +9,10 @@ class BlogPost extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content'];
+    protected $fillable = [
+        'title',
+        'content'
+    ];
     
     public function comments()
     {
